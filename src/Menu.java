b315948e613fc4e1610445b73c70d7d04a14b039
vipulkingkesari <49883145+vipulkingkesari@users.noreply.github.com>
2021-11-30 17:19:17 +0530

    Update Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -2,16 +2,8 @@
 import java.awt.*;
 import javax.swing.*;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Vaibhav Sikarwar
- */
+ 
 public class Menu extends JPanel {
 
     /**
