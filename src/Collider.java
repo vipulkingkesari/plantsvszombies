597d9b068170e9e21371f6ef2a9ace375b04fcfd
vipulkingkesari <49883145+vipulkingkesari@users.noreply.github.com>
2021-11-30 17:20:10 +0530

    Update Collider.java

--- src/Collider.java
+++ src/Collider.java
@@ -6,9 +6,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-/**
- * Created by Armin on 6/25/2016.
- */
+
 public class Collider extends JPanel implements MouseListener {
 
     ActionListener al;
